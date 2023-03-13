adc1bfa4fec1166ab7355e1c71e690472bc9d658
Alberto Pascau <88436049+Albrtito@users.noreply.github.com>
2023-03-13 16:50:52 +0100

    Pseudo personal implementation: Personal loop for test3.

--- Phase_1/exercise2.py
+++ Phase_1/exercise2.py
@@ -10,8 +10,16 @@ class Dlist2(SList):
     def fix_loop(self):
         """The purpose of this method is to detect loops in the list and
         break them.
-        * We can only have one loop on each list: Cause each node is only referencint
-        another one. (can´t reference more than one)"""
+        * We can only have one loop on each list: Cause each node is only referencing
+        another one. (can´t reference more than one)
+        * We can´t just go to the end of the list and set it´s reference to None because if there is a loop there´ll be
+        no end
+        IDEA:
+        1. Use 1 while to go through each node until we reach None(if there was a None)
+        2. For each of the nodes use another while to compare the reference of the node to each of the previous nodes
+        3. If the reference matches with any of the previous nodes -> We have a loop
+        4. When finding a loop. Set the reference of the starting loop node to None and return True
+        5. If we don´t find any loops then we´ll reach None and return False. Without making changes"""
 
         if self.isEmpty():  # list empty
             return False
@@ -20,15 +28,16 @@ class Dlist2(SList):
 
         ITnode = self._head
         while ITnode is not None:  # Check for the end of the list. If we get to an element that is
-            # None we know there
-            # is an end
+            # None we know there is an end
             ITcheck = self._head  # We start by Iterating a check variable.
             while ITcheck is not ITnode:  # Check that the ITnode does not reference
                 # to a previous node. Check all previous elements to ITnode.
                 ITcheck = ITcheck.next  # Keep iterating the check until we reach the ITnode
-            # If the while stops we enter this if. If the while stopped
-            if ITcheck == ITnode.next:
+
+            if ITcheck == ITnode.next: # If the check node is the same as the reference of the ITnode we know the
+                # ITnode is creating a loop. We return True and break the loop (by setting it´s reference to None)
+                ITnode.next = None
                 return True
-            ITnode = ITnode.next
+            ITnode = ITnode.next # Iteration of the first while
 
         return False
