622f9d7528a14aa619c121dfcd776a68fe994b33
Ignaciol0 <117018561+Ignaciol0@users.noreply.github.com>
2023-03-27 20:56:42 +0200

    Update Exercise1.py

--- Phase_1/Exercise1.py
+++ Phase_1/Exercise1.py
@@ -1,3 +1,4 @@
+
 import unittest
 from phase1 import SList2
 from slistH import SList
@@ -7,12 +8,12 @@ class Test(unittest.TestCase):
 
     def setUp(self):
 	     self.list = SList()
-         self.reference = SList()
-
+           self.reference = SList()
+    
     def testempty(self):
 	      list = SList2(self.list)
-          check = self.reference
-          self.assertEqual(len(list), len(check), "Fail: len in a length of length==0")
+            check = self.reference
+            self.assertEqual(len(list), len(check), "Fail: len in a length of length==0")
      
     def test_one_element(self):
           list = SList2(self.list)
@@ -82,28 +83,60 @@ class Test(unittest.TestCase):
           check = self.reference
           check.addLast(1)
           check.addLast(2)
+
+class Test3(unittest.TestCase):
+
+    def setUp(self):
+	     self.list = SList()
+           self.reference = SList()
+
+    def testempty(self):
+	      list = SList2(self.list)
+            check = self.reference
+            self.assertEqual(len(list), len(check), "Fail: len in a length of length==0")
+     
+    def test_one_element(self):
+          self.list.addFirst(1)
+          list = SList2(self.list)
+          list.delLargestSeq()
+          check = self.reference
+          check.addFirst(1)
+          self.assertEqual(len(list),len(check), "Fail: len in a length of length==1")
+
+      def test_left(self):
+          list = self.list
+          list.addLast(1)
+          list.addLast(1)
+          list.addLast(1)
+          list.addLast(1)
+          list.addLast(2)
+          list.addLast(3)
+          list = SList2(list)
+          list.leftrightShift(True,3)
+          check = self.reference
+          check.addLast(1)
+          check.addLast(2)
+          check.addLast(3)
+          check.addLast(1)
+          check.addLast(1)
+          check.addLast(1)
+          self.assertEqual(len(list),len(check), "Fail: DeleteSequence didn't work")
+      def test_right(self):
+          list = self.list
+          list.addLast(1)
+          list.addLast(1)
+          list.addLast(1)
+          list.addLast(1)
+          list.addLast(2)
+          list.addLast(3)
+          list = SList2(list)
+          list.leftrightShift(False,3)
+          check = self.reference
+          check.addLast(1)
+          check.addLast(2)
+          check.addLast(3)
+          check.addLast(1)
+          check.addLast(1)
+          check.addLast(1)
+          self.assertEqual(len(list),len(check), "Fail: DeleteSequence didn't work")
     
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-if __name__ == "__main__":
-    unittest.main()
